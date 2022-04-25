dae1c1fc3d029662825b6bacfc62c7f29ef4f5dd
Chris Jacobs <chrisjacobs@multimediallc.com>
2022-04-25 15:06:10 -0700

    disabled some unused libs

--- build.sh
+++ build.sh
@@ -46,37 +46,37 @@ fi
 # Plugins from github
 [ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle
 cd ~/.vim/bundle
-[ ! -d TaskList.vim ]            && git clone https://github.com/vim-scripts/TaskList.vim.git
+# [ ! -d TaskList.vim ]            && git clone https://github.com/vim-scripts/TaskList.vim.git
 [ ! -d badwolf ]                 && git clone https://github.com/sjl/badwolf.git
 [ ! -d bclose.vim ]              && git clone https://github.com/rbgrouleff/bclose.vim.git
 [ ! -d ctrlp.vim ]               && git clone https://github.com/kien/ctrlp.vim.git
-[ ! -d gundo.vim ]               && git clone https://github.com/sjl/gundo.vim.git
+# [ ! -d gundo.vim ]               && git clone https://github.com/sjl/gundo.vim.git
 [ ! -d matchit ]                 && git clone https://github.com/tmhedberg/matchit.git
 [ ! -d minibufexpl.vim ]         && git clone https://github.com/fholgado/minibufexpl.vim.git
-[ ! -d mru.vim ]                 && git clone https://github.com/vim-scripts/mru.vim.git
+# [ ! -d mru.vim ]                 && git clone https://github.com/vim-scripts/mru.vim.git
 [ ! -d nerdtree ]                && git clone https://github.com/scrooloose/nerdtree.git
 [ ! -d python-mode ]             && git clone https://github.com/klen/python-mode.git
 [ ! -d rainbow_parentheses.vim ] && git clone https://github.com/kien/rainbow_parentheses.vim.git
 [ ! -d supertab ]                && git clone https://github.com/ervandew/supertab.git
-[ ! -d tagbar ]                  && git clone https://github.com/majutsushi/tagbar.git
-[ ! -d vim-afterimage ]          && git clone https://github.com/tpope/vim-afterimage.git
-[ ! -d vim-arduino ]             && git clone https://github.com/tclem/vim-arduino.git
+# [ ! -d tagbar ]                  && git clone https://github.com/majutsushi/tagbar.git
+# [ ! -d vim-afterimage ]          && git clone https://github.com/tpope/vim-afterimage.git
+# [ ! -d vim-arduino ]             && git clone https://github.com/tclem/vim-arduino.git
 [ ! -d vim-commentary ]          && git clone https://github.com/tpope/vim-commentary.git
 [ ! -d vim-easymotion ]          && git clone https://github.com/Lokaltog/vim-easymotion.git
-[ ! -d vim-eunuch ]              && git clone https://github.com/tpope/vim-eunuch.git
+# [ ! -d vim-eunuch ]              && git clone https://github.com/tpope/vim-eunuch.git
 [ ! -d vim-fugitive ]            && git clone https://github.com/tpope/vim-fugitive.git
 [ ! -d vim-gitgutter ]           && git clone https://github.com/airblade/vim-gitgutter.git
 [ ! -d vim-markdown ]            && git clone https://github.com/tpope/vim-markdown.git
-[ ! -d vim-pastie ]              && git clone https://github.com/tpope/vim-pastie.git
+# [ ! -d vim-pastie ]              && git clone https://github.com/tpope/vim-pastie.git
 [ ! -d vim-powerline ]           && git clone https://github.com/Lokaltog/vim-powerline.git
 [ ! -d vim-repeat ]              && git clone https://github.com/tpope/vim-repeat.git
 [ ! -d vim-sensible ]            && git clone https://github.com/tpope/vim-sensible.git
-[ ! -d vim-sleuth ]              && git clone https://github.com/tpope/vim-sleuth.git
+# [ ! -d vim-sleuth ]              && git clone https://github.com/tpope/vim-sleuth.git
 [ ! -d vim-speeddating ]         && git clone https://github.com/tpope/vim-speeddating.git
-[ ! -d vim-stylus ]              && git clone https://github.com/wavded/vim-stylus.git
+# [ ! -d vim-stylus ]              && git clone https://github.com/wavded/vim-stylus.git
 [ ! -d vim-surround ]            && git clone https://github.com/tpope/vim-surround.git
 [ ! -d vim-unimpaired ]          && git clone https://github.com/tpope/vim-unimpaired.git
-#[ ! -d vim-distinguished.vim ]   && git clone https://github.com/Lokaltog/vim-distinguished.git
+# [ ! -d vim-distinguished.vim ]   && git clone https://github.com/Lokaltog/vim-distinguished.git
 
 # Colors! :D
 cd ~/.vim
