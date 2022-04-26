44c4b1895f547645e65aa44e471abdf1cd7375d4
Chris Jacobs <chrisjacobs@multimediallc.com>
2022-04-26 09:36:27 -0700

    way cleaner output, and fewer one-liners where comparing lines not needed

--- build.sh
+++ build.sh
@@ -4,6 +4,14 @@
 REPO_DIR="${REPO_DIR:-code}"
 VIMRC_LINK_TARGET="${REPO_DIR}/vim_stuffs/vimrc"
 
+# let's not make people hate us
+if [ -f ~/.vimrc -a ! -L ~/.vimrc ]; then
+  echo "ERROR!" >&2
+  echo "Your ~/.vimrc file is not a symlink.  As a safety precaution you must first move (ideally" >&2
+  echo "to a backup location) ~/.vimrc before this program is allowed to manage your vim setup" >&2
+  exit 1
+fi
+
 
 # help
 if [ "$1" == "-h" ]; then
@@ -13,34 +21,46 @@ if [ "$1" == "-h" ]; then
   echo "                   then clones the dependant repos and colorpack, then finally backs"
   echo "                   up and links your .vimrc to ~/${REPO_DIR}/vim_stuffs/vimrc"
   echo "  -u  Update mode: Pulls in each repo it finds under ~/.vim, then runs as normal"
-  echo "  -c  Clean mode : Removes ~/.vim and the ~/.vimrc symlink, then runs as normal"
+  echo "  -c  Clean mode : Removes ~/.vim and the ~/.vimrc symlink, but not the"
+  echo "                   ~/${REPO_DIR}/vim_stuffs repo"
   echo "  -h  Help mode  : This help info"
+  exit 0
+fi
+
+# dep check
+if ! which wget >/dev/null; then
+  echo "Requires wget" >&2
+  exit 2
 fi
 
 # update mode
 if [ "$1" == "-u" ]; then
+  echo "Updating repos in parallel"
+  if [ ! -d ~/.vim ]; then
+    echo "Creating ~/.vim"
+    mkdir ~/.vim
+  fi
   cd ~/.vim/
   for dir in $(find . -name ".git" -type d); do
-    pushd $(dirname $dir)
-    git pull &
-    popd
+    rel_dir=$(dirname $dir)
+    echo "Updating ${rel_dir}"
+    pushd "${rel_dir}" >/dev/null
+    git pull >/dev/null &
+    popd >/dev/null
   done
   wait
+  echo "Finished updates"
 fi
 
 # clean mode
 if [ "$1" == "-c" ]; then
   rm -rf ~/.vim
   rm -f ~/.vimrc
+  echo "All clean"
+  exit 0
 fi
 
-# dep check
-if ! which wget 2>/dev/null; then
-  echo "Requires wget"
-  exit 1
-fi
-
-# dependencies
+# dependencies for non-mac
 if ! uname -a | grep -q ^Darwin; then
   for pkg in 'curl' 'git' 'exuberant-ctags'; do
   if ! dpkg -l | grep -q $pkg; then
@@ -51,12 +71,16 @@ fi
 
 # project dir
 cd ~
-[ ! -d "${REPO_DIR}" ] && mkdir "${REPO_DIR}"
+if [ ! -d "${REPO_DIR}" ]; then
+  echo "Creating dir ${REPO_DIR}"
+  mkdir "${REPO_DIR}"
+fi
 
 # teh repo
 cd ${REPO_DIR}
 if [ ! -d "vim_stuffs" ]; then
-  git clone https://github.com/zonk1024/vim_stuffs.git
+  echo "Cloning vim_stuffs"
+  git clone https://github.com/zonk1024/vim_stuffs.git 2>/dev/null
 fi
 
 # relink .vimrc
@@ -66,54 +90,53 @@ ln -s ${VIMRC_LINK_TARGET} .vimrc
 
 # Pathogen
 [ ! -d ~/.vim/autoload ] && mkdir -p ~/.vim/autoload
-[ ! -f ~/.vim/autoload/pathogen.vim ] && wget -O ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim
+if [ ! -f ~/.vim/autoload/pathogen.vim ]; then
+  echo "Downloading pathogen"
+  wget -O ~/.vim/autoload/pathogen.vim https://raw.github.com/tpope/vim-pathogen/master/autoload/pathogen.vim 2>/dev/null
+fi
 
 # Plugins from github
 [ ! -d ~/.vim/bundle ] && mkdir -p ~/.vim/bundle
 cd ~/.vim/bundle
-[ ! -d badwolf ]                 && git clone https://github.com/sjl/badwolf.git
-[ ! -d bclose.vim ]              && git clone https://github.com/rbgrouleff/bclose.vim.git
-[ ! -d ctrlp.vim ]               && git clone https://github.com/kien/ctrlp.vim.git
-[ ! -d matchit ]                 && git clone https://github.com/tmhedberg/matchit.git
-[ ! -d minibufexpl.vim ]         && git clone https://github.com/fholgado/minibufexpl.vim.git
-[ ! -d nerdtree ]                && git clone https://github.com/scrooloose/nerdtree.git
-[ ! -d python-mode ]             && git clone https://github.com/klen/python-mode.git
-[ ! -d rainbow_parentheses.vim ] && git clone https://github.com/kien/rainbow_parentheses.vim.git
-[ ! -d supertab ]                && git clone https://github.com/ervandew/supertab.git
-[ ! -d vim-commentary ]          && git clone https://github.com/tpope/vim-commentary.git
-[ ! -d vim-easymotion ]          && git clone https://github.com/Lokaltog/vim-easymotion.git
-[ ! -d vim-fugitive ]            && git clone https://github.com/tpope/vim-fugitive.git
-[ ! -d vim-gitgutter ]           && git clone https://github.com/airblade/vim-gitgutter.git
-[ ! -d vim-markdown ]            && git clone https://github.com/tpope/vim-markdown.git
-[ ! -d vim-powerline ]           && git clone https://github.com/Lokaltog/vim-powerline.git
-[ ! -d vim-repeat ]              && git clone https://github.com/tpope/vim-repeat.git
-[ ! -d vim-sensible ]            && git clone https://github.com/tpope/vim-sensible.git
-[ ! -d vim-speeddating ]         && git clone https://github.com/tpope/vim-speeddating.git
-[ ! -d vim-surround ]            && git clone https://github.com/tpope/vim-surround.git
-[ ! -d vim-unimpaired ]          && git clone https://github.com/tpope/vim-unimpaired.git
-# [ ! -d TaskList.vim ]            && git clone https://github.com/vim-scripts/TaskList.vim.git
-# [ ! -d gundo.vim ]               && git clone https://github.com/sjl/gundo.vim.git
-# [ ! -d mru.vim ]                 && git clone https://github.com/vim-scripts/mru.vim.git
-# [ ! -d tagbar ]                  && git clone https://github.com/majutsushi/tagbar.git
-# [ ! -d vim-afterimage ]          && git clone https://github.com/tpope/vim-afterimage.git
-# [ ! -d vim-arduino ]             && git clone https://github.com/tclem/vim-arduino.git
-# [ ! -d vim-distinguished.vim ]   && git clone https://github.com/Lokaltog/vim-distinguished.git
-# [ ! -d vim-eunuch ]              && git clone https://github.com/tpope/vim-eunuch.git
-# [ ! -d vim-pastie ]              && git clone https://github.com/tpope/vim-pastie.git
-# [ ! -d vim-sleuth ]              && git clone https://github.com/tpope/vim-sleuth.git
-# [ ! -d vim-stylus ]              && git clone https://github.com/wavded/vim-stylus.git
+echo "Cloning any missing repos in parallel"
+[ ! -d badwolf ]                 && echo "Cloning: badwolf"                 && git clone https://github.com/sjl/badwolf.git 2>/dev/null &
+[ ! -d bclose.vim ]              && echo "Cloning: bclose.vim"              && git clone https://github.com/rbgrouleff/bclose.vim.git 2>/dev/null &
+[ ! -d ctrlp.vim ]               && echo "Cloning: ctrlp.vim"               && git clone https://github.com/kien/ctrlp.vim.git 2>/dev/null &
+[ ! -d matchit ]                 && echo "Cloning: matchit"                 && git clone https://github.com/tmhedberg/matchit.git 2>/dev/null &
+[ ! -d minibufexpl.vim ]         && echo "Cloning: minibufexpl.vim"         && git clone https://github.com/fholgado/minibufexpl.vim.git 2>/dev/null &
+[ ! -d nerdtree ]                && echo "Cloning: nerdtree"                && git clone https://github.com/scrooloose/nerdtree.git 2>/dev/null &
+[ ! -d python-mode ]             && echo "Cloning: python-mode"             && git clone https://github.com/klen/python-mode.git 2>/dev/null &
+[ ! -d rainbow_parentheses.vim ] && echo "Cloning: rainbow_parentheses.vim" && git clone https://github.com/kien/rainbow_parentheses.vim.git 2>/dev/null &
+[ ! -d supertab ]                && echo "Cloning: supertab"                && git clone https://github.com/ervandew/supertab.git 2>/dev/null &
+[ ! -d vim-commentary ]          && echo "Cloning: vim-commentary"          && git clone https://github.com/tpope/vim-commentary.git 2>/dev/null &
+[ ! -d vim-easymotion ]          && echo "Cloning: vim-easymotion"          && git clone https://github.com/Lokaltog/vim-easymotion.git 2>/dev/null &
+[ ! -d vim-fugitive ]            && echo "Cloning: vim-fugitive"            && git clone https://github.com/tpope/vim-fugitive.git 2>/dev/null &
+[ ! -d vim-gitgutter ]           && echo "Cloning: vim-gitgutter"           && git clone https://github.com/airblade/vim-gitgutter.git 2>/dev/null &
+[ ! -d vim-markdown ]            && echo "Cloning: vim-markdown"            && git clone https://github.com/tpope/vim-markdown.git 2>/dev/null &
+[ ! -d vim-powerline ]           && echo "Cloning: vim-powerline"           && git clone https://github.com/Lokaltog/vim-powerline.git 2>/dev/null &
+[ ! -d vim-repeat ]              && echo "Cloning: vim-repeat"              && git clone https://github.com/tpope/vim-repeat.git 2>/dev/null &
+[ ! -d vim-sensible ]            && echo "Cloning: vim-sensible"            && git clone https://github.com/tpope/vim-sensible.git 2>/dev/null &
+[ ! -d vim-speeddating ]         && echo "Cloning: vim-speeddating"         && git clone https://github.com/tpope/vim-speeddating.git 2>/dev/null &
+[ ! -d vim-surround ]            && echo "Cloning: vim-surround"            && git clone https://github.com/tpope/vim-surround.git 2>/dev/null &
+[ ! -d vim-unimpaired ]          && echo "Cloning: vim-unimpaired"          && git clone https://github.com/tpope/vim-unimpaired.git 2>/dev/null &
+wait
+echo "Done cloning repos"
 
 # Colors! :D
 cd ~/.vim
-
 if [ ! -f ColorSamplerPack.zip ]; then
   echo "Downloading ColorSamplerPack.zip"
-  curl -o ColorSamplerPack.zip 'https://www.vim.org/scripts/download_script.php?src_id=18915' 2>/dev/null >&2
+  curl -o ColorSamplerPack.zip 'https://www.vim.org/scripts/download_script.php?src_id=18915' 2>/dev/null
   if [ $? -ne 0 ]; then
-    echo "FAILED TO DOWNLOAD ColorSamplerPack.zip"
-    exit 1
+    echo "FAILED TO DOWNLOAD ColorSamplerPack.zip" >&2
+    exit 3
   fi
 fi
 
 [ ! -d plugin ] && mkdir plugin
-[ ! -f plugin/color_sample_pack.vim ] && unzip ColorSamplerPack.zip >/dev/null
+if [ ! -f plugin/color_sample_pack.vim ]; then
+  echo "Unzipping ColorSamplerPack.zip"
+  unzip ColorSamplerPack.zip >/dev/null
+fi
+
+echo "Done"
